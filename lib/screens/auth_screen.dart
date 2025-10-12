4f2b298cf1bb25aeaa698a530cd7cab825c352d1
Slazzical <jeremy.ouma@strathmore.edu>
2025-10-13 00:02:41 +0300

    feat: front-end login and signup pages

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:plant_disease_detection/widgets/login_form.dart'; // We'll create these files
+import 'package:plant_disease_detection/widgets/signup_form.dart';
+
+enum AuthMode { login, signup }
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  AuthMode _authMode = AuthMode.login;
+
+  void _switchAuthMode() {
+    setState(() {
+      if (_authMode == AuthMode.login) {
+        _authMode = AuthMode.signup;
+      } else {
+        _authMode = AuthMode.login;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_authMode == AuthMode.login ? 'Login' : 'Sign Up'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Placeholder for a logo or app title
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
+                child: const Text(
+                  'Verdia',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              _authMode == AuthMode.login ? const LoginForm() : const SignUpForm(),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: _switchAuthMode,
+                child: Text(
+                  _authMode == AuthMode.login
+                      ? 'Don\'t have an account? Sign Up'
+                      : 'Already have an account? Login',
+                  style: const TextStyle(color: Colors.green),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
